5fda8dff4588bcafc9a2378e11a10773b4de394f
soulboy <2146546761@qq.com>
2026-01-23 15:33:56 +0800

    Fix: Updated next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,  // 启用 React 严格模式，帮助你捕捉潜在问题
+  swcMinify: true,        // 使用 SWC 来进行代码压缩，提升性能
+};
 
 module.exports = nextConfig;
